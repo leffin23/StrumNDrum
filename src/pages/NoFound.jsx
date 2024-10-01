a7aa9a374afaf4be8e29ef8d811acbd58e4c55fb
leffin23 <mini.dasha.12@gmail.com>
2024-10-02 00:00:32 +0300

    Updating not found page

--- src/pages/NoFound.jsx
+++ src/pages/NoFound.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"
-
+import error404 from "../assets/404purp.png"
 
 const NoFound = () => {
   return (
     <div className="error-page">
         <p>Ooops.. This page doesn't exist. </p>
         <div> Go back to <Link to="/">Home</Link></div>
-        <img className="error-img" src="/src/assets/404purp.png" alt="error 404"></img>
+        <img className="error-img" src={error404} alt="error 404"></img>
 
     </div>
   )
